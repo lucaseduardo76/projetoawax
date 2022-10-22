3b5e36383cbbc1a123a37e585b74a4f002802401
Lucas Eduardo <lesds2001@gmail.com>
2022-10-22 01:38:55 -0300

    adicionando slide

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,7 +8,32 @@
                 }
             });
         };
+
+
+
+
+        /*var timer = new Timer(function() {
+        // your function here
+        }, 5000);
+
+        // switch interval to 10 seconds
+        timer.reset(10000);
+
+        // stop the timer
+        timer.stop();
+
+        // start the timer
+        timer.start();
+                
+        var timer = new Timer(()=>{
+            if(n > 2){
+                n = 0
+            }  
+            document.querySelector(`.slie`).style.marginLeft =  `calc(-${n}00vw)`;       
+            n++ }, 2000)*/
+
         
+
         function jump(){
             if(n > 2){
                 n = 0
@@ -20,18 +45,24 @@
         function jumpOne(){
             document.querySelector(`.slie`).style.marginLeft =  `calc(-000vw)`;
             n = 1; 
+            clearInterval(myTimer)
+            myTimer = setInterval(jump, 10000);
         }
         function jumpTwo(){
             document.querySelector(`.slie`).style.marginLeft =  `calc(-100vw)`;
             n = 2;
+            clearInterval(myTimer)
+            myTimer = setInterval(jump, 10000);
         }
         function jumpThree(){
             document.querySelector(`.slie`).style.marginLeft =  `calc(-200vw)`;
             n = 3;
+            clearInterval(myTimer)
+            myTimer = setInterval(jump, 10000);
         }
 
 
-        setInterval(jump, 10000);
+        let myTimer = setInterval(jump, 10000);
         
         let n = 0;
       
